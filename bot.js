2d883dacc67a6c1a6d3bbfe3a47109fa1ca3f716
TyRXTN <44922386+TyRXTN@users.noreply.github.com>
2018-12-06 15:56:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "00";
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
+var prefix = "d";
+var adminprefix = '.'
+const developers = ["441344394625548300"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`:white_check_mark: Changing The Playing Mode To **${argresult}** :white_check_mark:  `)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`:white_check_mark: Successfully Changing The Watching Mode To **${argresult}** :white_check_mark: `)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`:white_check_mark: Successfully Changing The Listening Mode To **${argresult}** :white_check_mark: `)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/One");
+      message.channel.send(`:white_check_mark: Successfully Changing The Streaming Mode To **${argresult}** :white_check_mark: `)
+  }
+  if (message.content.startsWith(adminprefix + 'setn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'seta')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
 });
 
-client.on('ready', async() => {
-var server = "520161821919936513"; // ايدي السررفر
-var channel = "520161919684837388";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**xSh3Dy_ NUMBER ONE**')
-    },305);
-})
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "00say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
 client.login(process.env.BOT_TOKEN);
